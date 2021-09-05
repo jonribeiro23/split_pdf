4553ac47f28b3f78981de0282c895924789ac346
jonribeiro23 <44584409+jonribeiro23@users.noreply.github.com>
2021-09-05 14:37:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
+from split import split_pdf
+from get_files import get_files_name
+from pdf import ocr_file
 from tqdm import tqdm
+import subprocess
 
 # https://towardsdatascience.com/extracting-text-from-scanned-pdf-using-pytesseract-open-cv-cd670ee38052
 # https://www.thepythoncode.com/article/extract-text-from-images-or-scanned-pdf-python
 
-inputpdf = PdfFileReader(open("bb0121_edital.pdf", "rb"))
+menu = 0
 
-for i in tqdm(range(inputpdf.numPages)):
-    output = PdfFileWriter()
-    output.addPage(inputpdf.getPage(i))
-    with open("document-page%s.pdf" % i, "wb") as outputStream:
-        output.write(outputStream)
+while menu != 9:
+    print("1 - Dividir pdf")
+    print("2 - Renomear pdf")
+    print("9 - Sair")
+    menu = int(input("Escolher: "))
+
+    if menu == 1:
+        path_to_file = str(input("Caminho/nome do arquivo: "))
+        path_to_save = str(input("Pasta para salvar: "))
+        split_pdf(path_to_file, path_to_save)
+    
+    if menu == 2:
+        path_to_file = str(input("Caminho para os arquivos: "))
+        files = get_files_name(path_to_file)
+        for file in tqdm(files):
+            print(f"{path_to_file}/{file}")
+            subprocess.run(["python", "pdf.py", "-s", "ANY", "-i", f"{path_to_file}/{file}", "-o", "output.pdf"])
+
+    if menu == 3:
+        break
