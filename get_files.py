1a2cdb2f13000b17c78c77b08e916113b0e99755
jonribeiro23 <44584409+jonribeiro23@users.noreply.github.com>
2021-09-05 14:40:04 -0300

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -1,1 +1,11 @@
+from os import listdir
+from os.path import isfile, join
+
+def get_files_name(path):
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+# res = get_files_name('./01-09')
+
+# for i in res:
+#     print(i)
 
